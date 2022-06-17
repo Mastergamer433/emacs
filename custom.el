41e0c14fed02342cb6f6fa0469fa4d57efe3c67b
Elis Odenhage <teknikgillaren@gmail.com>
2022-06-17 20:39:49 +0200

    2022-06-17 20:40

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit counsel-projectile projectile exwm hydra evil-collection evil doom-themes hass general hue-control hue-control\.el helpful counsel ivy-rich which-key rainbow-delimiters all-the-icons doom-modeline ivy command-log-mode use-package))
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
